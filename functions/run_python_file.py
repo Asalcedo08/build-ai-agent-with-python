68049a69dac44123e0dedc2bf91e310305da2996
Aidensalcedo08 <Aidensalcedo08@gmail.com>
2026-05-03 10:31:47 -0500

    Add function for running python

---
+++ functions/run_python_file.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+
+
+def run_python_file(working_directory, file_path, args=None):
+    try:
+        #Get working and target paths
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+
+        #Check path validity
+        if not os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        
+        if not os.path.isfile(target_dir):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        
+        if target_dir[-3:] != ".py":
+            return f'Error: "{file_path}" is not a Python file'
+        
+        #Building command to run
+        command = ["python", target_dir]
+        if args != None:
+            command.extend(args)
+
+        #Running subprocess
+        completed_process = subprocess.run(command, cwd = working_dir_abs, capture_output=True, text=True, timeout=30, )
+        
+        #Build complete string
+        complete_string = ""
+        if completed_process.returncode != 0:
+            complete_string += f"Process exited with code {completed_process.returncode}\n"
+
+        if completed_process.stdout == "" and completed_process.stderr == "":
+            complete_string += "No output produced"
+        else:
+            if completed_process.stdout:
+                complete_string += f"STDOUT: {completed_process.stdout}"
+            if completed_process.stderr:
+                complete_string += f"\nSTDERR: {completed_process.stderr}"
+
+        return complete_string
+    except Exception as e:
+        return f"Error: {e}"
